e55183b420f8fbf1eb8004ab0b61691120a8a0bc
Daffa Damar <daffaardhika@gmail.com>
2023-09-10 08:48:25 +0700

    edit file name

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
-import Home from "./pages/home"
+import Home from "./pages/Home"
 import Login from "./pages/Login";
 
 function App() {
